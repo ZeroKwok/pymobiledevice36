ccf94471ae3773c8a1c906019da9f0bf077dcfc7
lixiaojin <xiaojin-1@163.com>
2024-12-06 18:17:41 +0800

    提交测试

---
+++ example/location.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append(".\..")
+sys.path.append(".\..\pymobiledevice3")
+from pymobiledevice3.services.dvt.instruments.location_simulation import LocationSimulation
+
+
+def test_set_location(dvt):
+    """
+    Test set location.
+    """
+    # set to liberty island
+    LocationSimulation(dvt).set(40.690008, -74.045843)
+
+
+def test_clear_location(dvt):
+    """
+    Test clear location simulation
+    """
+    # set to liberty island
+    LocationSimulation(dvt).clear()
